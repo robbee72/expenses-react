846aff1f415ee145de159f85fc4310404d606084
robbee72 <krobnson@gmail.com>
2018-10-28 19:45:02 -0500

    setupdb env

--- src/tests/setUpTests.js
+++ src/tests/setUpTests.js
@@ -1,3 +1,9 @@
-import { configure } from 'enzyme';
+import DotEnv from 'dotenv';
 import Adapter from 'enzyme-adapter-react-16';
+import {configure} from 'enzyme';
+
 configure({ adapter: new Adapter() });
+
+DotEnv.config({
+  path: '.env.test'
+});
